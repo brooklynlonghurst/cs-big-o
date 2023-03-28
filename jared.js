6195817cfd72ed566429cb4db88dec50b5947221
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-03-28 15:54:06 -0600

    finished cs-big-o lab

---
+++ jared.js
@@ -0,0 +1,11 @@
+function func(str) {
+    console.log(str)
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i]
+        console.log(char)
+    }
+    console.log(char)
+    
+}
+
+func('asdfasdkghkjgkgkjgkjgkjkuhkhkhkuhkuhiuhgkjgkjgkjgkjgkjgfasdf')
